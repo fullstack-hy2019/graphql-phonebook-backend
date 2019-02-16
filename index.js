bbdce86c06714745cf06d513390b751ffc93cf08
Matti Luukkainen <mluukkai@iki.fi>
2019-02-16 20:04:52 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,65 @@
+const { ApolloServer, gql } = require('apollo-server')
+
+const persons = [
+  {
+    name: "Arto Hellas",
+    phone: "040-123543",
+    street: "Tapiolankatu 5 A",
+    city: "Espoo"
+  },
+  {
+    name: "Matti Luukkainen",
+    phone: "040-432342",
+    street: "Malminkaari 10 A",
+    city: "Helsinki"
+  },
+  {
+    name: "Venla Ruuska",
+    street: "Nallemäentie 22 C",
+    city: "Helsinki"
+  },
+]
+
+const typeDefs = gql`
+  type Address {
+    street: String!
+    city: String! 
+  }
+
+  type Person {
+    name: String!
+    phone: String
+    address: Address!
+  }
+
+  type Query {
+    personCount: Int!
+    allPersons: [Person!]!
+    findPerson(name: String!): Person
+  }
+`
+
+const resolvers = {
+  Query: {
+    personCount: () => persons.length,
+    allPersons: () => persons,
+    findPerson: (root, args) => persons.find(p => p.name === args.name)
+  },
+  Person: {
+    address: (root) => {
+      return { 
+        street: root.street,
+        city: root.city
+      }
+    }
+  }
+}
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+})
+
+server.listen().then(({ url }) => {
+  console.log(`Server ready at ${url}`)
+})
